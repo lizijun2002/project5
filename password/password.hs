231238b488a61e352a1a4e72829124f0c37f924f
lizijun2002 <165897715+lizijun2002@users.noreply.github.com>
2025-11-23 23:32:34 -0500

    Implement Monad and Functor instances for PwdOp

--- password/password.hs
+++ password/password.hs
@@ -1,17 +1,35 @@
 module Password where
 
-newtype PwdOp a = ???
+newtype PwdOp a = PwdOp (String -> (a, String))
+
+instance Functor PwdOp where
+  fmap f (PwdOp g) = PwdOp $ \s ->
+    let (a, s') = g s
+    in (f a, s')
+
+instance Applicative PwdOp where
+  pure x = PwdOp $ \s -> (x, s)
+  (PwdOp ff) <*> (PwdOp fa) = PwdOp $ \s ->
+    let (f, s')  = ff s
+        (a, s'') = fa s'
+    in (f a, s'')
+
+instance Monad PwdOp where
+  return = pure
+  (PwdOp fa) >>= f = PwdOp $ \s ->
+    let (a, s')  = fa s
+        PwdOp fb = f a
+    in fb s'
 
 setPassword :: String -> PwdOp ()
--- Your code here
+setPassword pwd = PwdOp $ \_ ->
+  ((), pwd)
 
 checkPassword :: String -> PwdOp Bool
--- Your code here
+checkPassword input = PwdOp $ \currentPwd ->
+  (input == currentPwd, currentPwd)
 
 runPwdOp :: PwdOp a -> a
--- Your code here
-
-
-
-
-
+runPwdOp (PwdOp f) =
+  let (result, _) = f ""
+  in result
